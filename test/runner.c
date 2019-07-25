ca04d0776245a01d1247a7b9223523cf41f81c62
Abhishek Chakravarti <abhishek@taranjali.org>
2019-07-25 19:03:36 +0530

    add rough test for pcr_mempool_alloc()
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/runner.c
+++ test/runner.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "../src/api.h"
 
 
@@ -37,14 +38,33 @@ int do_do_compare(int *a, int *b, pcr_exception ex)
 }
 
 
+char *make_str(const char *str, pcr_exception ex)
+{
+    auto pcr_exception x;
+
+    pcr_exception_try (x) {
+        register size_t len = strlen(str);
+        char *bfr = pcr_mempool_alloc(len + 1, x);
+        strncpy(bfr, str, len);
+        bfr[len] = '\0';
+
+        return bfr;
+    }
+
+    pcr_exception_unwind(ex);
+}
+
+
 int main(void)
 {
     pcr_exception x;
 
     pcr_exception_try (x) {
+        char *bfr = make_str("Hello, world! Goodbye, world!", x);
+        printf("bfr = %s\n", bfr);
+
         int a = 5;
         int b = 6;
-
         printf("a == b: %d\n", do_do_compare(&a, &b, x));
         printf("a == b: %d\n", do_do_compare(0, &b, x));
     }
