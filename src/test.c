c330bccbcd55098b0f9912b189cf727b6156fb0d
Abhishek Chakravarti <abhishek@taranjali.org>
2019-07-25 19:53:29 +0530

    define pcr_testsuite vector
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/test.c
+++ src/test.c
@@ -16,10 +16,11 @@ extern pcr_testcase *pcr_testcase_new(pcr_unittest *test, const char *desc,
     pcr_exception x;
     pcr_exception_try (x) {
         pcr_testcase *tc = pcr_mempool_alloc(sizeof *tc, x);
-        tc->test = test;
 
         const size_t len = strlen(desc);
+        tc->test = test;
         tc->desc = pcr_mempool_alloc(len + 1, x);
+
         strncpy(tc->desc, desc, len);
         tc->desc[len] = '\0';
 
@@ -35,5 +36,33 @@ extern bool pcr_testcase_run(pcr_testcase *ctx, pcr_exception ex)
     pcr_assert_handle(ctx, ex);
 
     return ctx->test();
+}
+
+
+struct pcr_testsuite {
+    pcr_testcase **tests;
+    size_t cap;
+    size_t len;
+};
+
+
+extern pcr_testsuite *pcr_testsuite_new(const char *name, pcr_exception ex)
+{
+}
+
+
+extern uint64_t pcr_testsuite_len(const pcr_testsuite *ctx, pcr_exception ex)
+{
+}
+
+
+extern void pcr_testsuite_push(pcr_testsuite *ctx, const pcr_testcase *tc,
+                                    pcr_exception ex)
+{
+}
+
+
+extern uint64_t pcr_testsuite_run(pcr_testsuite *ctx)
+{
 }
 
