789af4aa3616817e71787b211ecc109a0050d7b0
Abhishek Chakravarti <abhishek@taranjali.org>
2019-07-27 11:51:43 +0530

    implement pcr_vector_copy()
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/vector.c
+++ src/vector.c
@@ -6,6 +6,7 @@ struct pcr_vector {
     size_t sz;
     size_t len;
     size_t cap;
+    size_t ref;
 };
 
 
@@ -17,7 +18,7 @@ extern pcr_vector *pcr_vector_new(const size_t elemsz, pcr_exception ex)
         pcr_vector *ctx = pcr_mempool_alloc(sizeof *ctx, x);
 
         ctx->sz = elemsz;
-        ctx->len = 0;
+        ctx->len = ctx->ref = 0;
         ctx->cap = 4;
         ctx->payload = pcr_mempool_alloc(elemsz * ctx->cap, x);
 
@@ -26,5 +27,16 @@ extern pcr_vector *pcr_vector_new(const size_t elemsz, pcr_exception ex)
 
     pcr_exception_unwind(ex);
     return NULL;
+}
+
+
+extern pcr_vector *pcr_vector_copy(const pcr_vector *ctx, pcr_exception ex)
+{
+    pcr_assert_handle(ctx, ex);
+
+    pcr_vector *hnd = (pcr_vector *) ctx;
+    hnd->ref++;
+
+    return hnd;
 }
 
