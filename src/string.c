fce44d5ddddbd2d9bba7bf935508da82a680f5d5
Abhishek Chakravarti <abhishek@taranjali.org>
2019-07-29 08:46:46 +0530

    implement pcr_string_add()
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/string.c
+++ src/string.c
@@ -42,6 +42,20 @@ extern size_t pcr_string_len(const pcr_string *ctx, pcr_exception ex)
 extern pcr_string *pcr_string_add(const pcr_string *ctx, const pcr_string *add,
                                         pcr_exception ex)
 {
+    pcr_assert_handle(ctx && add, ex);
+
+
+    pcr_exception_try (x) {
+        const size_t llen = utf8len(ctx);
+        const size_t rlen = utf8len(add);
+
+        pcr_string *cat = pcr_mempool_alloc(sizeof *cat * (llen + rlen + 1), x);
+        (void) utf8ncpy(cat, ctx, llen);
+        return utf8ncat(cat, add, rlen);
+    }
+
+    pcr_exception_unwind(ex);
+    return NULL;
 }
 
 
