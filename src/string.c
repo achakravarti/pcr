435d41e1f4fdce631b47d3c150ec186157fc2dd4
Abhishek Chakravarti <abhishek@taranjali.org>
2019-07-29 08:19:02 +0530

    implement pcr_string_new()
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

---
+++ src/string.c
@@ -0,0 +1,56 @@
+#include "../lib/utf8.h"
+#include "./api.h"
+
+
+extern pcr_string *pcr_string_new(const char *cstr, pcr_exception ex)
+{
+    pcr_assert_handle(cstr, ex);
+
+    pcr_exception_try (x) {
+        const size_t len = utf8len(cstr) + 1;
+        pcr_string *ctx = pcr_mempool_alloc(sizeof *ctx * len, x);
+
+        (void) utf8cpy(ctx, cstr);
+        ctx[len] = '\0';
+
+        return ctx;
+    }
+
+    pcr_exception_unwind(ex);
+    return NULL;
+}
+
+
+extern pcr_string *pcr_string_copy(const pcr_string *ctx, pcr_exception ex)
+{
+}
+
+
+extern size_t pcr_string_len(const pcr_string *ctx, pcr_exception ex)
+{
+}
+
+
+extern pcr_string *pcr_string_add(const pcr_string *ctx, const pcr_string *add,
+                                        pcr_exception ex)
+{
+}
+
+
+extern size_t pcr_string_search(const pcr_string *ctx, const pcr_string *sub,
+                                        pcr_exception ex)
+{
+}
+
+
+extern pcr_string *pcr_string_substr(const pcr_string *ctx, size_t start,
+                                        size_t end, pcr_exception ex)
+{
+}
+
+
+extern pcr_string *pcr_string_split(const pcr_string *ctx, size_t where,
+                                        pcr_exception ex)
+{
+}
+
