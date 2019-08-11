873b5de224b95035f1ba6447c0526b8d95cb9ce1
Abhishek Chakravarti <abhishek@taranjali.org>
2019-08-11 21:36:12 +0530

    remove most rough tests
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/runner.c
+++ test/runner.c
@@ -52,21 +52,6 @@ char *make_str(const char *str, pcr_exception ex)
 }
 
 
-static bool test_dummy(void)
-{
-    pcr_exception_try (x) {
-        int a = 5, b = 6;
-        compare(&a, &b, x);
-    }
-
-    pcr_exception_catch (PCR_EXCEPTION_STATE) {
-        return true;
-    }
-
-    return false;
-}
-
-
 int main(void)
 {
     pcr_log_open("test.log", true);
@@ -76,20 +61,7 @@ int main(void)
         char *bfr = make_str("Hello, world! Goodbye, world!", x);
         printf("bfr = %s\n", bfr);
 
-        pcr_testcase *tc = pcr_testcase_new(&test_dummy, "Dummy Test", x);
-
-        pcr_testsuite *ts = pcr_testsuite_new("Rough Tests", x);
-        pcr_testsuite_push(ts, tc, x);
-        pcr_testsuite_push(ts, tc, x);
-        pcr_testsuite_push(ts, tc, x);
-
-        pcr_testharness_init("test3.log", x);
-        pcr_testharness_push(ts, x);
-        pcr_testharness_push(ts, x);
-        pcr_testharness_push(ts, x);
-        pcr_testharness_push(ts, x);
-        pcr_testharness_push(ts, x);
-        pcr_testharness_push(ts, x);
+        pcr_testharness_init("bld/test.log", x);
         pcr_testharness_push(pcr_string_testsuite(x), x);
         pcr_testharness_run(x);
         pcr_testharness_exit();
