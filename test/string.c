343e7dbeff0c88bb5da07295ee69b63954517360
Abhishek Chakravarti <abhishek@taranjali.org>
2019-08-11 10:19:55 +0530

    add pcr_string_new() test #2
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/string.c
+++ test/string.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "./suites.h"
 
 
@@ -6,8 +7,21 @@ static bool test_new_1(void)
 {
     pcr_exception_try (x) {
         pcr_string *test = pcr_string_new("", x);
-
         return test && !*test;
+    }
+
+    return false;
+}
+
+
+#define DESC_NEW_2 "pcr_string_new() can create an ASCII string"
+static bool test_new_2(void)
+{
+    pcr_exception_try (x) {
+        const char *expect = "Hello, world!";
+        pcr_string *test = pcr_string_new(expect, x);
+
+        return test && !strcmp(test, expect);
     }
 
     return false;
@@ -21,6 +35,9 @@ pcr_string_testsuite(pcr_exception ex)
         pcr_testsuite *ts = pcr_testsuite_new("PCR String (pcr_string)", x);
 
         pcr_testcase *tc = pcr_testcase_new(&test_new_1, DESC_NEW_1, x);
+        pcr_testsuite_push(ts, tc, x);
+
+        tc = pcr_testcase_new(&test_new_2, DESC_NEW_2, x);
         pcr_testsuite_push(ts, tc, x);
 
         return ts;
