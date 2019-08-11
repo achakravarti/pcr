71aba5ef63093dd183e004c6acef68d0dc012000
Abhishek Chakravarti <abhishek@taranjali.org>
2019-08-11 12:40:35 +0530

    add pcr_string_len() test cases
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/string.c
+++ test/string.c
@@ -128,6 +128,65 @@ static bool test_copy_4(void)
 }
 
 
+/******************************************************************************
+ * pcr_string_len() test cases
+ */
+
+
+#define DESC_LEN_1 "pcr_string_len() reports 0 for a null string"
+static bool test_len_1(void)
+{
+    pcr_exception_try (x) {
+        pcr_string *test = pcr_string_new("", x);
+        return pcr_string_len(test, x) == 0;
+    }
+
+    return false;
+}
+
+
+#define DESC_LEN_2 "pcr_string_len() reports the length of an ASCII string"
+static bool test_len_2(void)
+{
+    pcr_exception_try (x) {
+        pcr_string *test = pcr_string_new("Hello, world!", x);
+        return pcr_string_len(test, x) == 13;
+    }
+
+    return false;
+}
+
+
+#define DESC_LEN_3 "pcr_string_len() reports the length of a Unicode string"
+static bool test_len_3(void)
+{
+    pcr_exception_try (x) {
+        pcr_string *test = pcr_string_new("Привет, мир!", x);
+        return pcr_string_len(test, x) == 12;
+    }
+
+    return false;
+}
+
+
+#define DESC_LEN_4 "pcr_string_len() throws PCR_EXCEPTION_HANDLE if passed a" \
+                   " NULL pointer for @ctx"
+static bool test_len_4(void)
+{
+    pcr_exception_try (x) {
+        (void) pcr_string_len(NULL, x);
+    }
+
+    pcr_exception_catch (PCR_EXCEPTION_HANDLE) {
+        return true;
+    }
+
+    return false;
+}
+
+
+
+
 
 extern pcr_testsuite *
 pcr_string_testsuite(pcr_exception ex)
@@ -151,6 +210,15 @@ pcr_string_testsuite(pcr_exception ex)
         tc = pcr_testcase_new(&test_copy_3, DESC_COPY_3, x);
         pcr_testsuite_push(ts, tc, x);
         tc = pcr_testcase_new(&test_copy_4, DESC_COPY_4, x);
+        pcr_testsuite_push(ts, tc, x);
+
+        tc = pcr_testcase_new(&test_len_1, DESC_LEN_1, x);
+        pcr_testsuite_push(ts, tc, x);
+        tc = pcr_testcase_new(&test_len_2, DESC_LEN_2, x);
+        pcr_testsuite_push(ts, tc, x);
+        tc = pcr_testcase_new(&test_len_3, DESC_LEN_3, x);
+        pcr_testsuite_push(ts, tc, x);
+        tc = pcr_testcase_new(&test_len_4, DESC_LEN_4, x);
         pcr_testsuite_push(ts, tc, x);
 
         return ts;
