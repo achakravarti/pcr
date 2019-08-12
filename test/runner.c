cc0a98da5aa3eb122c095d99de64b0a7998b02c8
Abhishek Chakravarti <abhishek@taranjali.org>
2019-08-12 09:02:56 +0530

    clean up dead code
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/runner.c
+++ test/runner.c
@@ -3,74 +3,15 @@
 #include "./suites.h"
 
 
-int compare(int *a, int *b, pcr_exception ex)
-{
-    pcr_assert_state(a && b, ex);
-    return *a == *b;
-}
-
-
-
-int do_compare(int *a, int *b, pcr_exception ex)
-{
-    pcr_exception_try (x) {
-        return compare(a, b, x);
-    }
-
-    pcr_exception_unwind(ex);
-    return 0;
-}
-
-
-int do_do_compare(int *a, int *b, pcr_exception ex)
-{
-    pcr_exception_try (x) {
-        return do_compare(a, b, x);
-    }
-
-    pcr_exception_catch (PCR_EXCEPTION_HANDLE) {
-        printf("Bad handle passed...\n");
-    }
-
-    pcr_exception_unwind(ex);
-    return 0;
-}
-
-
-char *make_str(const char *str, pcr_exception ex)
-{
-    pcr_exception_try (x) {
-        register size_t len = strlen(str) + 1;
-        char *bfr = pcr_mempool_alloc(len, x);
-        strncpy(bfr, str, len);
-
-        return bfr;
-    }
-
-    pcr_exception_unwind(ex);
-    return NULL;
-}
-
-
 int main(void)
 {
     pcr_log_open("test.log", true);
 
     pcr_exception_try (x) {
-
-        char *bfr = make_str("Hello, world! Goodbye, world!", x);
-        printf("bfr = %s\n", bfr);
-
         pcr_testharness_init("bld/test.log", x);
         pcr_testharness_push(pcr_string_testsuite(x), x);
         pcr_testharness_run(x);
         pcr_testharness_exit();
-
-
-        int a = 5;
-        int b = 6;
-        printf("a == b: %d\n", do_do_compare(&a, &b, x));
-        printf("a == b: %d\n", do_do_compare(0, &b, x));
     }
 
     pcr_exception_catchall {
