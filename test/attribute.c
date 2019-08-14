e1995e948af6e6d1320951b66a2cfb66dff50498
Abhishek Chakravarti <abhishek@taranjali.org>
2019-08-14 19:41:54 +0530

    add pcr_attribute_copy() test cases
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/attribute.c
+++ test/attribute.c
@@ -98,6 +98,29 @@ static bool test_new_4(pcr_string **desc, pcr_exception ex)
 
 static bool test_new_5(pcr_string **desc, pcr_exception ex)
 {
+    *desc = "pcr_attribute_new() can create a Unicode string attribute";
+
+    pcr_exception_try (x) {
+        const pcr_string *KEY = "bar";
+        const pcr_string *VALUE = "Привет, мир!";
+        const PCR_ATTRIBUTE TYPE  = PCR_ATTRIBUTE_TEXT;
+        pcr_attribute *test = pcr_attribute_new(TYPE, KEY, VALUE, x);
+
+        PCR_ATTRIBUTE type = pcr_attribute_type(test, x);
+        pcr_string *key = pcr_attribute_key(test, x);
+        pcr_string *value = pcr_attribute_value(test, x);
+
+        return type == TYPE && !pcr_string_cmp(key, KEY, x)
+               && !pcr_string_cmp(value, VALUE, x);
+    }
+
+    pcr_exception_unwind(ex);
+    return false;
+}
+
+
+static bool test_new_6(pcr_string **desc, pcr_exception ex)
+{
     *desc = "pcr_attribute_new() throws PCR_EXCEPTION_STRING if passed a NULL"
             " pointer for @key";
 
@@ -114,7 +137,7 @@ static bool test_new_5(pcr_string **desc, pcr_exception ex)
 }
 
 
-static bool test_new_6(pcr_string **desc, pcr_exception ex)
+static bool test_new_7(pcr_string **desc, pcr_exception ex)
 {
     *desc = "pcr_attribute_new() throws PCR_EXCEPTION_STRING if passed a null"
             " string for @key";
@@ -132,7 +155,7 @@ static bool test_new_6(pcr_string **desc, pcr_exception ex)
 }
 
 
-static bool test_new_7(pcr_string **desc, pcr_exception ex)
+static bool test_new_8(pcr_string **desc, pcr_exception ex)
 {
     *desc = "pcr_attribute_new() does not throw PCR_EXCEPTION_HANDLE if passed"
             " a null pointer for @value when @field is PCR_ATTRIBUTE_NULL";
@@ -147,7 +170,7 @@ static bool test_new_7(pcr_string **desc, pcr_exception ex)
 }
 
 
-static bool test_new_8(pcr_string **desc, pcr_exception ex)
+static bool test_new_9(pcr_string **desc, pcr_exception ex)
 {
     *desc = "pcr_attribute_new() throws PCR_EXCEPTION_HANDLE if passed a null"
             " pointer for @value when @field is not PCR_ATTRIBUTE_NULL";
@@ -166,13 +189,154 @@ static bool test_new_8(pcr_string **desc, pcr_exception ex)
 
 
 /******************************************************************************
+ * pcr_attribute_copy() test cases
+ */
+
+
+static bool test_copy_1(pcr_string **desc, pcr_exception ex)
+{
+    *desc = "pcr_attribute_copy() can copy a null attribute";
+
+    pcr_exception_try (x) {
+        pcr_attribute *test = pcr_attribute_new(PCR_ATTRIBUTE_NULL, "key",
+                                                NULL, x);
+        pcr_attribute *copy = pcr_attribute_copy(test, x);
+
+        PCR_ATTRIBUTE type = pcr_attribute_type(copy, x);
+        pcr_string *key = pcr_attribute_key(copy, x);
+        void *value = pcr_attribute_value(copy, x);
+
+        return type == PCR_ATTRIBUTE_NULL && !pcr_string_cmp(key, "key", x)
+                       && !value;
+    }
+
+    pcr_exception_unwind(ex);
+    return false;
+}
+
+
+static bool test_copy_2(pcr_string **desc, pcr_exception ex)
+{
+    *desc = "pcr_attribute_copy() can copy an int attribute";
+
+    pcr_exception_try (x) {
+        const pcr_string *KEY = "foo";
+        const int64_t VALUE = -1024;
+        const PCR_ATTRIBUTE TYPE  = PCR_ATTRIBUTE_INT;
+        pcr_attribute *test = pcr_attribute_new(TYPE, KEY, &VALUE, x);
+        pcr_attribute *copy = pcr_attribute_copy(test, x);
+
+        PCR_ATTRIBUTE type = pcr_attribute_type(copy, x);
+        pcr_string *key = pcr_attribute_key(copy, x);
+        int64_t *value = pcr_attribute_value(copy, x);
+
+        return type == TYPE && !pcr_string_cmp(key, KEY, x) && *value == VALUE;
+    }
+
+    pcr_exception_unwind(ex);
+    return false;
+}
+
+
+static bool test_copy_3(pcr_string **desc, pcr_exception ex)
+{
+    *desc = "pcr_attribute_copy() can copy a float attribute";
+
+    pcr_exception_try (x) {
+        const pcr_string *KEY = "frobnoz";
+        const double VALUE = -3.141592654;
+        const PCR_ATTRIBUTE TYPE  = PCR_ATTRIBUTE_FLOAT;
+        pcr_attribute *test = pcr_attribute_new(TYPE, KEY, &VALUE, x);
+        pcr_attribute *copy = pcr_attribute_copy(test, x);
+
+        PCR_ATTRIBUTE type = pcr_attribute_type(copy, x);
+        pcr_string *key = pcr_attribute_key(copy, x);
+        double *value = pcr_attribute_value(copy, x);
+
+        return type == TYPE && !pcr_string_cmp(key, KEY, x)
+                       && fabs(*value - VALUE) < DBL_EPSILON;
+    }
+
+    pcr_exception_unwind(ex);
+    return false;
+}
+
+
+static bool test_copy_4(pcr_string **desc, pcr_exception ex)
+{
+    *desc = "pcr_attribute_copy() can copy an ASCII string attribute";
+
+    pcr_exception_try (x) {
+        const pcr_string *KEY = "bar";
+        const pcr_string *VALUE = "Hello, world!";
+        const PCR_ATTRIBUTE TYPE  = PCR_ATTRIBUTE_TEXT;
+        pcr_attribute *test = pcr_attribute_new(TYPE, KEY, VALUE, x);
+        pcr_attribute *copy = pcr_attribute_copy(test, x);
+
+        PCR_ATTRIBUTE type = pcr_attribute_type(copy, x);
+        pcr_string *key = pcr_attribute_key(copy, x);
+        pcr_string *value = pcr_attribute_value(copy, x);
+
+        return type == TYPE && !pcr_string_cmp(key, KEY, x)
+               && !pcr_string_cmp(value, VALUE, x);
+    }
+
+    pcr_exception_unwind(ex);
+    return false;
+}
+
+
+static bool test_copy_5(pcr_string **desc, pcr_exception ex)
+{
+    *desc = "pcr_attribute_copy() can copy a Unicode string attribute";
+
+    pcr_exception_try (x) {
+        const pcr_string *KEY = "bar";
+        const pcr_string *VALUE = "Привет, мир!";
+        const PCR_ATTRIBUTE TYPE  = PCR_ATTRIBUTE_TEXT;
+        pcr_attribute *test = pcr_attribute_new(TYPE, KEY, VALUE, x);
+        pcr_attribute *copy = pcr_attribute_copy(test, x);
+
+        PCR_ATTRIBUTE type = pcr_attribute_type(copy, x);
+        pcr_string *key = pcr_attribute_key(copy, x);
+        pcr_string *value = pcr_attribute_value(copy, x);
+
+        return type == TYPE && !pcr_string_cmp(key, KEY, x)
+               && !pcr_string_cmp(value, VALUE, x);
+    }
+
+    pcr_exception_unwind(ex);
+    return false;
+}
+
+
+static bool test_copy_6(pcr_string **desc, pcr_exception ex)
+{
+    *desc = "pcr_attribute_copy() throws PCR_EXCEPTION_HANDLE if passed a NULL"
+            " pointer for @ctx";
+
+    pcr_exception_try (x) {
+        (void) pcr_attribute_copy(NULL, x);
+    }
+
+    pcr_exception_catch (PCR_EXCEPTION_HANDLE) {
+        return true;
+    }
+
+    pcr_exception_unwind(ex);
+    return false;
+}
+
+
+/******************************************************************************
  * pcr_attribute_testsuite() interface
  */
 
 
 static pcr_unittest *unit_tests[] = {
     test_new_1, test_new_2, test_new_3, test_new_4, test_new_5, test_new_6,
-    test_new_7, test_new_8
+    test_new_7, test_new_8, test_new_9, test_copy_1, test_copy_2, test_copy_3,
+    test_copy_4, test_copy_5, test_copy_6
 };
 
 
